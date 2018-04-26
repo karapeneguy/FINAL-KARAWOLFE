30b7347af2401220184375e98b42c77d6f6c8948
karapeneguy <helpdesk@helps-imac-206.smc.loyno.edu>
2018-04-26 10:44:08 -0500

    added javascript last day of class

---
+++ templates/footer.php
@@ -0,0 +1,33 @@
+
+
+
+
+<footer>
+    <div class="blackboxfooter">
+        
+        <h2>© Kara Wolfe 2018</h2>
+        </div>
+    </footer>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  </body>
+</html>
+
+<script src="assets/scripts/main.js"></script>
+
+</body>
+
+    
+    
+
+
+
+
+</html>
