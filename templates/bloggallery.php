c0a9df7eb613a2d1fdc1841c75bf468d0cf3c59b
karapeneguy <helpdesk@helps-imac-206.smc.loyno.edu>
2018-05-01 10:32:36 -0500

    fixed blog page
    fixed post one

--- templates/bloggallery.php
+++ templates/bloggallery.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php"; 
 
 $gallery = array(
-    array("the intro", "bluewallfull.jpg", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", "blogpostone.php" ), 
+    array("the intro", "bluewallfull.jpg", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).", "postone.php" ), 
     array("idk JACK", "cactus.jpg", ),
     array("The Difference", "loyolasunset.jpg"),
  
@@ -38,7 +38,7 @@ foreach($gallery as $item){
 <P><?php echo $item[2] ?> </P>
     
     <!-- Read More --> 
-     <button><a href="blogpostpages/<?php echo $item[3] ?>" >Read More</a></button>
+     <button><a href="<?php echo $item[3] ?>" >Read More</a></button>
 
   
 </div>
